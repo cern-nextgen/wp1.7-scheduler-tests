d85a02a257b977c51d262c08ac8c12fedd7ef362
arkslobodkins <aslobodkins@smu.edu>
2024-11-11 14:43:18 +0100

    added spacepoint formation algorithm

--- src/TracccAlgorithm.cpp
+++ src/TracccAlgorithm.cpp
@@ -3,13 +3,16 @@
 
 #include <iostream>
 #include <traccc/io/read_cells.hpp>
+#include <traccc/io/read_detector.hpp>
 #include <traccc/io/read_detector_description.hpp>
+#include <traccc/seeding/spacepoint_formation_algorithm.hpp>
 
 StatusCode TracccAlgorithm::initialize() {
    return StatusCode::SUCCESS;
 }
 
 AlgorithmBase::AlgCoInterface TracccAlgorithm::execute(EventContext ctx) const {
+   //  add spacepoint formation and a seeding algorithm
    traccc::edm::silicon_cell_collection::host cells{m_mr};
    traccc::silicon_detector_description::host det_descr{m_mr};
 
@@ -23,6 +26,21 @@ AlgorithmBase::AlgCoInterface TracccAlgorithm::execute(EventContext ctx) const {
 
    auto measurements = m_clusterization(vecmem::get_data(cells), vecmem::get_data(det_descr));
    std::cout << "Number of measurements: " << measurements.size() << std::endl;
+
+   traccc::default_detector::host detector{m_mr};
+   // detector file, material file, grid file
+   traccc::io::read_detector(detector,
+                             m_mr,
+                             "geometries/odd/odd-detray_geometry_detray.json",
+                             "geometries/odd/odd-detray_material_detray.json",
+                             "geometries/odd/odd-detray_surface_grids_detray.json");
+
+   using spacepoint_formation_algorithm
+       = traccc::host::spacepoint_formation_algorithm<traccc::default_detector::host>;
+
+   spacepoint_formation_algorithm sf{m_mr};
+   spacepoint_formation_algorithm::output_type spacepoints{&m_mr};
+   spacepoints = sf(detector, vecmem::get_data(measurements));
 
    co_return StatusCode::SUCCESS;
 }
