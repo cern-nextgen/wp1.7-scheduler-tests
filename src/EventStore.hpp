687539d9e5c4bd868d24e98c62b7f3e75afd6e85
Eric Cano <Eric.Cano@cern.ch>
2025-06-06 17:10:10 +0200

    Adds per element locking to EventStore.
    There is also an incomplete startup of glbal locking

--- src/EventStore.hpp
+++ src/EventStore.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-
 #include <cassert>
 #include <map>
 #include <memory>
+#include <mutex>
+#include <shared_mutex>
 #include <string>
 #include <typeinfo>
 #include <utility>
@@ -47,13 +48,63 @@ private:
 };
 
 /**
+ * @brief Struct to hold the value and its associated mutex.
+ */
+struct ValueWithMutex {
+   std::unique_ptr<ObjectHolderBase> value;
+   mutable std::shared_mutex mutex;
+
+   // Default constructor
+   ValueWithMutex() = default;
+
+   // Move constructor
+   ValueWithMutex(ValueWithMutex&& other) noexcept
+       : value(std::move(other.value)) {
+       // Note: `mutex` is default-constructed for the new object
+   }
+
+   // Move assignment operator
+   ValueWithMutex& operator=(ValueWithMutex&& other) noexcept {
+       if (this != &other) {
+           value = std::move(other.value);
+           // Note: `mutex` is not moved; it remains default-constructed
+       }
+       return *this;
+   }
+
+   // Deleted copy constructor and copy assignment operator
+   ValueWithMutex(const ValueWithMutex&) = delete;
+   ValueWithMutex& operator=(const ValueWithMutex&) = delete;
+};
+
+/**
  * @brief Store for event data, allowing to record and retrieve objects by name. Objects can be any type. Type is also checked at retrieval time.
- * @note This class is not thread-safe.
- * @note The obecjt is returned by pointer.
- * @todo Get rid of pointer interface.
  */
 class EventStore {
 public:
+   using KeyType = std::string;
+   using ValueType = ValueWithMutex;
+
+   // Trivial constructor
+   EventStore() = default;
+
+   // Move constructor
+   EventStore(EventStore&& other) noexcept {
+      std::unique_lock<std::shared_mutex> lock(other.m_globalMutex); // Lock the source's global mutex
+      m_store = std::move(other.m_store);
+   }
+
+   // Move assignment operator
+   EventStore& operator=(EventStore&& other) noexcept {
+      if (this != &other) {
+         std::unique_lock<std::shared_mutex> lockThis(m_globalMutex, std::defer_lock);
+         std::unique_lock<std::shared_mutex> lockOther(other.m_globalMutex, std::defer_lock);
+         std::lock(lockThis, lockOther); // Lock both mutexes
+         m_store = std::move(other.m_store);
+      }
+      return *this;
+   }
+
    /**
     * @brief Test product presence in the store.
     * @tparam T product type
@@ -62,24 +113,32 @@ public:
     */
    template <typename T>
    bool contains(const std::string& name) const {
-      auto it = m_dataStore.find(name);
-      return (it != m_dataStore.end()) && (typeid(T) == it->second->get_type());
+      auto it = m_store.find(name);
+      if (it == m_store.end()) {
+         return false;
+      }
+      std::shared_lock<std::shared_mutex> lock(it->second.mutex); // Lock the element mutex
+      return typeid(T) == it->second.value->get_type();
    }
 
    /**
     * @brief Get product
     * @tparam T product type
-    * @param obj reference to pointer, ill be set to point to the product
+    * @param obj reference to pointer, will be set to point to the product
     * @param name Name of the product
     * @return StatusCode (FAILURE if the product is not present or of the wrong type)
     */
    template <typename T>
    StatusCode retrieve(const T*& obj, const std::string& name) {
-      if(!this->contains<T>(name)) {
+      auto it = m_store.find(name);
+      if (it == m_store.end()) {
          return StatusCode::FAILURE;
       }
-
-      obj = static_cast<const T*>(m_dataStore[name]->get_pointer());
+      std::shared_lock<std::shared_mutex> lock(it->second.mutex); // Lock the element mutex
+      if (typeid(T) != it->second.value->get_type()) {
+         return StatusCode::FAILURE;
+      }
+      obj = static_cast<const T*>(it->second.value->get_pointer());
       return StatusCode::SUCCESS;
    }
 
@@ -93,11 +152,15 @@ public:
    template <typename T>
    StatusCode record(std::unique_ptr<T>&& obj, const std::string& name) {
       // Cannot record multiple objects with the same name.
-      if(m_dataStore.find(name) != m_dataStore.end()) {
+      auto it = m_store.find(name);
+      if (it != m_store.end()) {
          return StatusCode::FAILURE;
       }
 
-      m_dataStore[name] = std::make_unique<ObjectHolder<T>>(std::move(obj));
+      ValueWithMutex newValue;
+      newValue.value = std::make_unique<ObjectHolder<T>>(std::move(obj));
+      std::unique_lock<std::shared_mutex> lock(m_globalMutex); // Lock the global mutex for write
+      m_store[name] = std::move(newValue);
       return StatusCode::SUCCESS;
    }
 
@@ -105,12 +168,13 @@ public:
     * @brief Clears the store and deletes all the products.
     */
    void clear() {
-      m_dataStore.clear();
+      std::unique_lock<std::shared_mutex> lock(m_globalMutex); // Lock the global mutex for write
+      m_store.clear();
    }
 
 private:
-   /// Map to hold the products by name.
-   std::map<std::string, std::unique_ptr<ObjectHolderBase>> m_dataStore;
+   mutable std::shared_mutex m_globalMutex; // Global lock for the map
+   std::map<KeyType, ValueType> m_store;    // The map storing the elements with their associated mutexes
 };
 
 /**
