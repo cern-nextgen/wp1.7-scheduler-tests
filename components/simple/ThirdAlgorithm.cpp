568d81d1e7870959d69f80e981f61dbec48fefce
Eric Cano <Eric.Cano@cern.ch>
2025-06-20 16:49:26 +0200

    Adds graphs to the third algorithm.

--- components/simple/ThirdAlgorithm.cpp
+++ components/simple/ThirdAlgorithm.cpp
@@ -10,6 +10,39 @@
 #include "../../tests/NVTXUtils.hpp"
 using WP17Scheduler::NVTXUtils::nvtxcolor;
 
+#include <cuda_runtime.h>
+
+// --- ThirdAlgorithmGraph Implementation ---
+ThirdAlgorithmGraph::ThirdAlgorithmGraph() {
+    std::lock_guard<std::mutex> lock(m_graphMutex);
+    CUDA_ASSERT(cudaGraphCreate(&m_graph, 0));
+    m_kernel5Params.func = kernel5Address();
+    m_kernel5Params.gridDim = dim3(1);
+    m_kernel5Params.blockDim = dim3(1);
+    m_kernel5Params.sharedMemBytes = 0;
+    m_kernel5Params.kernelParams = nullptr;
+    m_kernel5Params.extra = nullptr;
+    CUDA_ASSERT(cudaGraphAddKernelNode(&m_kernel5Node, m_graph, nullptr, 0, &m_kernel5Params));
+    m_hostFunctionParams.fn = notifyScheduler;
+    m_hostFunctionParams.userData = nullptr;
+    CUDA_ASSERT(cudaGraphAddHostNode(&m_HostFunctionNode, m_graph, &m_kernel5Node, 1, &m_hostFunctionParams));
+    CUDA_ASSERT(cudaGraphInstantiate(&m_graphExec, m_graph, nullptr, nullptr, 0));
+}
+ThirdAlgorithmGraph::~ThirdAlgorithmGraph() {
+    std::lock_guard<std::mutex> lock(m_graphMutex);
+    if (m_graphExec) cudaGraphExecDestroy(m_graphExec);
+    if (m_graph) cudaGraphDestroy(m_graph);
+}
+
+void ThirdAlgorithmGraph::launchGraph(cudaStream_t stream, Notification* notification) {
+    std::lock_guard<std::mutex> lock(m_graphMutex);
+    // Only update host function userData for this launch
+    m_hostFunctionParams.userData = notification;
+    CUDA_ASSERT(cudaGraphExecHostNodeSetParams(m_graphExec, m_HostFunctionNode, &m_hostFunctionParams));
+    CUDA_ASSERT(cudaGraphLaunch(m_graphExec, stream));
+}
+
+// --- ThirdAlgorithm Implementation ---
 ThirdAlgorithm::ThirdAlgorithm(bool verbose)
     : m_verbose(verbose) {}
 
@@ -48,6 +81,57 @@ AlgorithmBase::AlgCoInterface ThirdAlgorithm::execute(EventContext ctx) const {
     auto range2 = nvtx3::scoped_range{MEMBER_FUNCTION_NAME(ThirdAlgorithm) + " conclusion" + ctx.info(), nvtxcolor(ctx.eventNumber), nvtx3::payload{ctx.eventNumber}};
     co_return StatusCode::SUCCESS;
 }
+
+AlgorithmBase::AlgCoInterface ThirdAlgorithm::executeGraph(EventContext ctx) const {
+    if (m_verbose) {
+        std::cout << MEMBER_FUNCTION_NAME(ThirdAlgorithm) + " part1 start, " << ctx.info() << std::endl;
+    }
+    nvtx3::unique_range range{MEMBER_FUNCTION_NAME(ThirdAlgorithm) + " part1" + ctx.info(), nvtxcolor(ctx.eventNumber), nvtx3::payload{ctx.eventNumber}};
+    const int* input = nullptr;
+    SC_CHECK_YIELD(EventStoreRegistry::of(ctx).retrieve(input, AlgorithmBase::dependencies()[0]));
+    auto output = std::make_unique<int>(-1);
+    SC_CHECK_YIELD(EventStoreRegistry::of(ctx).record(std::move(output), AlgorithmBase::products()[0]));
+
+    if (m_verbose) {
+        std::cout << MEMBER_FUNCTION_NAME(ThirdAlgorithm) + " part1, " << ctx.info() << std::endl;
+    }
+    ctx.scheduler->setCudaSlotState(ctx.slotNumber, 2, false);
+    m_graphImpl.launchGraph(ctx.stream, new Notification{ctx, 2});
+    { auto r = std::move(range); } // End range
+    co_yield StatusCode::SUCCESS;
+
+    if (m_verbose) {
+        std::cout << MEMBER_FUNCTION_NAME(ThirdAlgorithm) + " conclusion, " << ctx.info() << std::endl;
+    }
+    auto range2 = nvtx3::scoped_range{MEMBER_FUNCTION_NAME(ThirdAlgorithm) + " conclusion" + ctx.info(), nvtxcolor(ctx.eventNumber), nvtx3::payload{ctx.eventNumber}};
+    co_return StatusCode::SUCCESS;
+}
+
+AlgorithmBase::AlgCoInterface ThirdAlgorithm::executeCachedGraph(EventContext ctx) const {
+    if (m_verbose) {
+        std::cout << MEMBER_FUNCTION_NAME(ThirdAlgorithm) + " part1 start, " << ctx.info() << std::endl;
+    }
+    nvtx3::unique_range range{MEMBER_FUNCTION_NAME(ThirdAlgorithm) + " part1" + ctx.info(), nvtxcolor(ctx.eventNumber), nvtx3::payload{ctx.eventNumber}};
+    const int* input = nullptr;
+    SC_CHECK_YIELD(EventStoreRegistry::of(ctx).retrieve(input, AlgorithmBase::dependencies()[0]));
+    auto output = std::make_unique<int>(-1);
+    SC_CHECK_YIELD(EventStoreRegistry::of(ctx).record(std::move(output), AlgorithmBase::products()[0]));
+
+    if (m_verbose) {
+        std::cout << MEMBER_FUNCTION_NAME(ThirdAlgorithm) + " part1, " << ctx.info() << std::endl;
+    }
+    ctx.scheduler->setCudaSlotState(ctx.slotNumber, 2, false);
+    m_graphContainer.launchGraph(ctx.stream, new Notification{ctx, 2});
+    { auto r = std::move(range); } // End range
+    co_yield StatusCode::SUCCESS;
+
+    if (m_verbose) {
+        std::cout << MEMBER_FUNCTION_NAME(ThirdAlgorithm) + " conclusion, " << ctx.info() << std::endl;
+    }
+    auto range2 = nvtx3::scoped_range{MEMBER_FUNCTION_NAME(ThirdAlgorithm) + " conclusion" + ctx.info(), nvtxcolor(ctx.eventNumber), nvtx3::payload{ctx.eventNumber}};
+    co_return StatusCode::SUCCESS;
+}
+
 
 StatusCode ThirdAlgorithm::finalize() {
     nvtx3::scoped_range range{MEMBER_FUNCTION_NAME(ThirdAlgorithm)};
--- components/simple/ThirdAlgorithm.hpp
+++ components/simple/ThirdAlgorithm.hpp
@@ -1,6 +1,27 @@
 #pragma once
 
 #include "AlgorithmBase.hpp"
+#include "CachingGraphContainer.hpp"
+
+#include <cuda_runtime_api.h>
+
+// Forward declarations
+struct Notification;
+
+class ThirdAlgorithmGraph {
+public:
+    ThirdAlgorithmGraph();
+    ~ThirdAlgorithmGraph();
+    void launchGraph(cudaStream_t stream, Notification* notification);
+private:
+    cudaGraph_t m_graph{};
+    cudaGraphExec_t m_graphExec{};
+    cudaGraphNode_t m_kernel5Node{};
+    cudaGraphNode_t m_HostFunctionNode{};
+    cudaKernelNodeParams m_kernel5Params{};
+    cudaHostNodeParams m_hostFunctionParams{};
+    std::mutex m_graphMutex;
+};
 
 class ThirdAlgorithm : public AlgorithmBase {
 public:
@@ -9,8 +30,13 @@ public:
 
     StatusCode initialize() override;
     AlgCoInterface execute(EventContext ctx) const override;
+    // Exceute straight is identical to execute (it will fall back to execute())
+    AlgCoInterface executeGraph(EventContext ctx) const override;
+    AlgCoInterface executeCachedGraph(EventContext ctx) const override;
     StatusCode finalize() override;
 
 private:
     bool m_verbose; // Whether verbose output is enabled
+    mutable ThirdAlgorithmGraph m_graphImpl; // Graph helper instance
+    mutable CachingGraphContainer<ThirdAlgorithmGraph> m_graphContainer; // Container for caching graph instances
 };
